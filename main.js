7e954f3e3945063344f8d4321187a021f8e3b68d
HaykBalbabyan <85069802+HaykBalbabyan@users.noreply.github.com>
2023-12-07 23:29:55 +0400

    ui init

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded',function () {
 
+    const API = 'http://192.168.18.119/'
+
     document.body.innerHTML = `<button id="toggle-button">Send Request</button>`;
 
-    fetch('/ledoff').then(response => {console.log(response)})
+    let ledStatus;
+
+    fetch(API + 'ledoff').then(response => {console.log(response)})
+
+    document.addEventListener('click', (e) => {
+        if (e.target.matches('#toggle-button')){
+
+        }
+    });
+
+
 
 });
