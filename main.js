08bee20138817157ff472ce92dea66fe7643075a
HaykBalbabyan <85069802+HaykBalbabyan@users.noreply.github.com>
2023-12-07 22:47:03 +0400

    js test

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-document.write(777777777);
+document.addEventListener('DOMContentLoaded',function () {
+
+    document.body.innerHTML = `<button id="toggle-button">Send Request</button>`;
+
+    fetch('/ledoff').then(response => {console.log(response)})
+
+});
