a3e84a8f14de46f927ba1c690b305994c64079ea
HaykBalbabyan <85069802+HaykBalbabyan@users.noreply.github.com>
2023-12-12 22:51:42 +0400

    FTP config

---
+++ FTP.js
@@ -0,0 +1,18 @@
+module.exports = {
+    access: {
+        host: '',
+        user: '',
+        password: '',
+    },
+    localDirectory: '',
+    serverDirectory: '',
+    ignore: [
+        'node_modules',
+        '.idea',
+        '.gitignore',
+        '.git',
+        '.github',
+        'FTP.js',
+        'upload.js'
+    ]
+};
